7b64595d30e6eecaa17f19c7fd408fbf90826ea6
S.Ignatyuk <signatyuk89@gmail.com>
2021-02-02 23:29:19 +0300

    spring security with in memory storage, with authorities READ and WRITE

--- src/main/java/com/config/SecurityConfig.java
+++ src/main/java/com/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.config;
 
+import com.model.Permission;
 import com.model.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,13 +23,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers(HttpMethod.GET, "/departments/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
-                .antMatchers(HttpMethod.POST, "/departments").hasRole(Role.ADMIN.name())
-                .antMatchers(HttpMethod.DELETE, "/departments/**").hasRole(Role.ADMIN.name())
-                .antMatchers(HttpMethod.GET, "/employees/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
-                .antMatchers(HttpMethod.POST, "/employees").hasRole(Role.ADMIN.name())
-                .antMatchers(HttpMethod.DELETE, "/employees/**").hasRole(Role.ADMIN.name())
-                .antMatchers(HttpMethod.PUT, "/employees/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.GET, "/departments/**").hasAuthority(Permission.READ.getPermission())
+                .antMatchers(HttpMethod.POST, "/departments").hasAuthority(Permission.WRITE.getPermission())
+                .antMatchers(HttpMethod.DELETE, "/departments/**").hasRole(Permission.WRITE.getPermission())
+                .antMatchers(HttpMethod.GET, "/employees/**").hasAnyRole(Permission.READ.getPermission())
+                .antMatchers(HttpMethod.POST, "/employees").hasRole(Permission.WRITE.getPermission())
+                .antMatchers(HttpMethod.DELETE, "/employees/**").hasRole(Permission.WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT, "/employees/**").hasRole(Permission.WRITE.getPermission())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -42,12 +43,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 User.builder()
                         .username("admin")
                         .password(passwordEncoder().encode("admin"))
-                        .roles(Role.ADMIN.name())
+                        .authorities(Role.ADMIN.getAuthorities())
                         .build(),
                 User.builder()
                         .username("user")
                         .password(passwordEncoder().encode("user"))
-                        .roles(Role.USER.name())
+                        .authorities(Role.USER.getAuthorities())
                         .build()
         );
     }
--- src/main/java/com/model/Role.java
+++ src/main/java/com/model/Role.java
@@ -1,6 +1,28 @@
 package com.model;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Role {
-    ADMIN,
-    USER
+    ADMIN(Stream.of(Permission.READ, Permission.WRITE).collect(Collectors.toSet())),
+    USER(Stream.of(Permission.READ).collect(Collectors.toSet()));
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    private final Set<Permission> permissions;
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getAuthorities() {
+        return getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+    }
 }
